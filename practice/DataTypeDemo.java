d5dc54eac96378d7b70e16d4607ace7cd8b0059c
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2022-05-15 07:28:29 +0530

    primitive and array half

--- practice/DataTypeDemo.java
+++ practice/DataTypeDemo.java
@@ -19,17 +19,28 @@ public class DataTypeDemo {
 		float coffee = 23.5f; //small or caps f
 		double tea = 46.887987987f;
 		
-		boolean a = true;
-		boolean b = false;
-		System.out.println(a);
+		boolean value = true;
+		boolean check = false;
+		System.out.println(check);
 		char pencil = 'z';
 		System.out.println(pencil);
 		char Pencil = 'k'; // quotes mandatory
 		System.out.println(Pencil);
 //		case sensitive
 		
-
+	int cup = 20;
+	System.out.println(cup);
+//	int keys = 20; declaration & initialization 
+	int keys; //declaration
+	keys = 20; //late intialization 
+	
+	System.out.println(keys);
 		
+	int a, b, c;            // Declares three ints, a, b, and c.
+	int a = 10, b = 10;     // Example of initialization
+	byte B = 22;            // initializes a byte type variable B.
+	double pi = 3.14159;    // declares and assigns a value of PI.
+	char a = 'a';           // the char variable a is initialized with value a
 
 	}
 
---
+++ practice/NonPrimitiveDemo.java
@@ -0,0 +1,23 @@
+package practice;
+
+public class NonPrimitiveDemo {
+
+	public static void main(String[] args) {
+		int a = 90;
+		int b = 93;
+		int c = 20;
+		int d = 56; // mark of student
+		//----ARRAY----------   []
+		int marks[] = new int[4];
+		marks[0]= 90;
+		marks[1] = 93;
+		marks[2] = 20;
+		marks[3] = 56;
+		System.out.println(marks.length);
+		System.out.println(marks[1]);
+		System.out.println(marks.toString());
+		
+
+	}
+
+}
