ca63c72929f8db86fd0564fcebb71c5a3dc9b737
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2022-05-15 06:50:34 +0530

    initial commit

---
+++ practice/DataTypeDemo.java
@@ -0,0 +1,36 @@
+package practice;
+
+public class DataTypeDemo {
+
+	public static void main(String[] args) {
+//		System.out.println("**********");
+//		parleG choclate = 10;
+//		datatype
+		byte choclate = 10;
+//		byte -> datatype
+//		choclate -> reference name/variable name
+//		= -> assigning
+//		10 -> value
+		int pizza = 20;
+		System.out.println(pizza);
+		
+		short burger = 150;
+		long pen = 25L;
+		float coffee = 23.5f; //small or caps f
+		double tea = 46.887987987f;
+		
+		boolean a = true;
+		boolean b = false;
+		System.out.println(a);
+		char pencil = 'z';
+		System.out.println(pencil);
+		char Pencil = 'k'; // quotes mandatory
+		System.out.println(Pencil);
+//		case sensitive
+		
+
+		
+
+	}
+
+}
