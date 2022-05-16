ca32f5904195f778aeae51354ec1f8b5a61ba96c
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2022-05-16 06:43:15 +0530

    string concept

---
+++ practice/StringDemo.java
@@ -0,0 +1,29 @@
+package practice;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+
+		String name =" Good morning ";
+		System.out.println(name);
+		System.out.println(name.length());
+		System.out.println(name.toLowerCase());
+		System.out.println(name.toUpperCase());
+		//		System.out.println(name + "team");
+		System.out.println(name.concat("team"));//append
+		System.out.println(name.charAt(2));
+		System.out.println(name.isEmpty());
+		System.out.println(name.substring(7)); //a part of
+		System.out.println(name.indexOf('d'));
+		System.out.println(name.indexOf('o'));
+		System.out.println(name.lastIndexOf('o'));
+		System.out.println(name);
+		System.out.println(name.trim());
+		//		System.out.println(name.);
+
+
+
+
+	}
+
+}
